19ac9e1ab5de003968827a1202fe562bd184883d
Revichi <111824316+Revichi@users.noreply.github.com>
2023-12-10 16:50:55 +0700

    Update index.py

--- index.py
+++ index.py
@@ -9,36 +9,28 @@ import joblib
 svm_model_path = 'Model/svm_model.pkl'
 svm_model = joblib.load(svm_model_path)
 
-    st.write("""
-    Harap Isi Data Sesuai Kolom, Data Tidak Boleh Kosong
-    """)
-    Area = st.number_input("Area", step=0.01)  # Set step ke nilai pecahan
-    Perimeter = st.number_input("Perimeter", step=0.01)
-    MajorAxisLength = st.number_input("Major_Axis_Length", step=0.01)
-    MinorAxisLength = st.number_input("Minor_Axis_Length", step=0.01)
-    Eccentricity = st.number_input("Eccentricity", step=0.01)
-    ConvexArea = st.number_input("Convex_Area", step=0.01)
+st.write("""
+Harap Isi Data Sesuai Kolom, Data Tidak Boleh Kosong
+""")
+Area = st.number_input("Area", step=0.01)  # Set step ke nilai pecahan
+Perimeter = st.number_input("Perimeter", step=0.01)
+MajorAxisLength = st.number_input("Major_Axis_Length", step=0.01)
+MinorAxisLength = st.number_input("Minor_Axis_Length", step=0.01)
+Eccentricity = st.number_input("Eccentricity", step=0.01)
+ConvexArea = st.number_input("Convex_Area", step=0.01)
 
-    columns = st.columns((2, 0.6, 2))
-    submit = columns[1].button("Submit")
+columns = st.columns((2, 0.6, 2))
+submit = columns[1].button("Submit")
 
 
   
-    if submit:
-        # Normalisasi data menggunakan fungsi normalisasi yang telah ditambahkan
-        normalized_data = aksi.normalisasi([Area, Perimeter, MajorAxisLength, MinorAxisLength, Eccentricity, ConvexArea])
+if submit:
+    # Normalisasi data menggunakan fungsi normalisasi yang telah ditambahkan
+    normalized_data = aksi.normalisasi([Area, Perimeter, MajorAxisLength, MinorAxisLength, Eccentricity, ConvexArea])
 
-        # Melakukan prediksi dengan model SVM
-        prediction = svm_model.predict([normalized_data])
+    # Melakukan prediksi dengan model SVM
+    prediction = svm_model.predict([normalized_data])
 
-        # Menampilkan hasil prediksi
-        st.header('Hasil Prediksi')
-        st.write(f'Model SVM memprediksi kelas: {prediction[0]}')
-
-
-# with st.sidebar:
-#     st.write('Link:')
-#     link = '[GitHub](https://github.com/Revichi/appdataset)'
-#     st.markdown(link, unsafe_allow_html=True)
-#     link = '[Jupyter Book](https://revichi.github.io/datamining/App.html?highlight=penambangan)'
-#     st.markdown(link, unsafe_allow_html=True)
+    # Menampilkan hasil prediksi
+    st.header('Hasil Prediksi')
+    st.write(f'Model SVM memprediksi kelas: {prediction[0]}')
